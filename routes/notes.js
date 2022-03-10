a2855ab2d7bcd7949ad091456a376d8f1fb9ee84
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-03-09 23:40:28 -0500

    Added get, post, and delete routes

---
+++ routes/notes.js
@@ -0,0 +1,41 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile
+} = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+
+  if ( title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json('Note added successfully');
+  } else {
+    res.error('Please add a note and title');
+  }
+});
+
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+      writeToFile('./db/db.json', result);
+      res.json(`Note ${noteId} has been deleted`);
+    });
+});
+
+module.exports = notes;
